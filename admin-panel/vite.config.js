6dfda0a00a2c9dc57ba4bda2a64ac6f68751def4
Rishav jain <jainrishavjain2007@gmail.com>
2026-01-28 13:01:50 +0530

    fix

--- admin-panel/vite.config.js
+++ admin-panel/vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0", // 👈 allow access via IP
+    port: 5174, // change if needed (5173 frontend, 5174 admin)
+  },
 });
