c02fce21a6ad82f4cd2fe184b8d9a61c904e4846
Rishav jain <jainrishavjain2007@gmail.com>
2026-01-28 12:40:24 +0530

    add tailwind

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
-    host: "0.0.0.0",   // 👈 allow access via IP
-    port: 5173,        // change if needed (5173 frontend, 5174 admin)
+    host: "0.0.0.0", // 👈 allow access via IP
+    port: 5173, // change if needed (5173 frontend, 5174 admin)
   },
 });
